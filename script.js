c75f97829413cdc941417f01e8160db6eecb5a99
Law <84906591+traffylawrence@users.noreply.github.com>
2024-02-12 20:04:32 +0800

    3rdcommit

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function changeImage(x,image)
 {
     if(x==1)
     {
-        image.src='/img/2pic.jpg';
+        image.src='img/2pic.jpg';
     }
     if(x==2)
     {
-        image.src='/img/pic.png';
+        image.src='img/pic.png';
     }
     
 }
